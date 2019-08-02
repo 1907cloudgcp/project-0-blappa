3bbd96a68491e59ae37b404ad13b7af9fcec967a
root <root@localhost.localdomain>
2019-08-02 01:22:32 -0400

    Update bank_p0.sql

--- bank_p0.sql
+++ bank_p0.sql
@@ -5,17 +5,10 @@ create table users (
   name varchar (100),
   email varchar (80),
   phnumber varchar (80),
-  login varchar (80),
+  birth_date varchar (80),
+  username varchar (80),
   pswd varchar (80),
   status boolean
 );
 
-insert into users values ('1111111','Bertrick Lappa','bertricklappa@gmail.com','4054762371','blappa','xx123','1');
-
-
-
-
-
-
-
-   
+insert into users values ('1111111','Bertrick Lappa','bertricklappa@gmail.com','4054762371','10/12/95','blappa','xx123','1');:   
